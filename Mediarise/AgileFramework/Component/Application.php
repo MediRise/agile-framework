733ef022f89a81402d73740ca9e5a285bad22838
Vitaliy Edifanov <vitaliy@edifanov.com>
2022-04-09 01:15:33 +0500

    little less changed

--- Mediarise/AgileFramework/Component/Application.php
+++ Mediarise/AgileFramework/Component/Application.php
@@ -2,6 +2,44 @@
 
 namespace Mediarise\AgileFramework\Component;
 
-class Application {
+class Application
+{
 
+    /**
+     * Base path Application
+     *
+     * @var
+     */
+    protected $basePath;
+
+    public function __construct($basePath = null)
+    {
+        if ($basePath) {
+            $this->setBasePath($basePath);
+        }
+    }
+
+    /**
+     * @param $basePath
+     * @return $this
+     */
+    public function setBasePath($basePath)
+    {
+        $this->basePath = rtrim($basePath, '\/');
+
+        return $this;
+    }
+
+    /**
+     * @param $path
+     * @return string
+     */
+    public function getBasePath($path = '')
+    {
+        if ($path) {
+            return $this->basePath . DIRECTORY_SEPARATOR . $path;
+        }
+
+        return $this->basePath;
+    }
 }
--- Mediarise/bootstrap/app.php
+++ Mediarise/bootstrap/app.php
@@ -2,6 +2,6 @@
 
 use Mediarise\AgileFramework\Component\Application;
 
-$app = new Application();
+$app = new Application($_ENV['APP_BASE_PATH'] ?? dirname(__DIR__));
 
 return $app;
